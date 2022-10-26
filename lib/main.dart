c86a446fe421aee3f2ab83192edc5635ce697c6d
hesham060 <heshamkhaled060@gmail.com>
2022-10-26 16:45:08 +0300

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'app/app.dart';
 
 void main() async {
   // we use one for make sure any future instace ready #2
+  WidgetsFlutterBinding.ensureInitialized();
   // this one hold all instance we should put before start program
   await initAppModule();
   runApp(MyApp());
